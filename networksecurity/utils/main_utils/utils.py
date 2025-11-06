50df1b3aeee168ef86d399eca1dd5b88f7bdf634
Nicholas_personal <nicho11688@gmail.com>
2025-11-06 23:30:07 +0700

    model trainer part 1

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -40,6 +40,24 @@ def save_object(file_path:str, obj: object) -> None:
         logging.info("Exited the save object method of MainUtils class")
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
+
+def load_object(file_path:str, obj:object)->object:
+    
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
 # def write_yaml(file_path:str,content:object, replace:bool = False)->None:
 #     try:
 #         if replace:

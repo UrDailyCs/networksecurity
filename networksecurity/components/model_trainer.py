8a7a1db42d53641a11561c55d7eb0a0e7ae3abee
Nicholas_personal <nicho11688@gmail.com>
2026-01-11 12:15:36 +0700

    dagshub

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -28,6 +28,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='UrDailyCs', repo_name='networksecurity', mlflow=True)
+
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact:DataTransformationArtifact):
         try:
